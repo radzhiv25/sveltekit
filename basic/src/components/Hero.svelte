82e82eeeddf238f4d8c84a2a5175d6cc758832bb
radzhiv <radzhivkrishna@gmail.com>
2024-02-15 23:53:32 +0530

    feat: hero section created

---
+++ basic/src/components/Hero.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Header from "./Header.svelte";
+import SectionWrapper from "./SectionWrapper.svelte";
+
+</script>
+<SectionWrapper>
+    <Header />
+    <h2 class="lg:text-6xl md:text-5xl sm:text-4xl text-2xl text-center max-w-[1200px] w-full mx-auto font-semibold my-10">
+            Unleash Infinite Creativity: Explore Endless Possibilities with <span class="text-transparent bg-clip-text bg-gradient-to-br from-pink-400 via-fuchsia-500 to-purple-600">Generative AI </span>Art.
+            <br>
+            <p class="line-through">No More Seaching.</p>
+            <p class="text-gray-600 lg:text-2xl text-lgr italic my-5">More of AI into works rather than numerous hours of making the art.</p>
+
+            <button class="py-2 px-3 bg-white border border-gray-300 font-medium md:text-lg  text-sm rounded-md shadow-md">Get Started</button>
+    </h2>
+</SectionWrapper>
